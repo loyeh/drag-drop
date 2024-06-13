c630f7f086d91ad4998301f47101e46b6d584ab0
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-06-13 19:47:58 +0330

    fixing problems

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 const dropItems = document.querySelectorAll(".dropItem");
+const dragItem = document.getElementById("dragItem");
 
 console.log(dragItem, dropItems);
 
-function dragoverHandler(ev) {
-  ev.preventDefault();
-  ev.dataTransfer.dropEffect = "move";
-  ev.target.classList.add("active");
-}
-
-function dropHandler(ev) {
-  ev.preventDefault();
-  // Get the id of the target and add the moved element to the target's DOM
-  const data = ev.dataTransfer.getData("image");
-  ev.target.appendChild(document.getElementById(data));
-  ev.target.classList.remove("active");
-
-  console.log(data);
-}
+dragItem.addEventListener("dragstart", dragStart);
+dragItem.addEventListener("dragend", dragEnd);
 
 dropItems.forEach((dropItem) => {
-  dropItem.addEventListener("dragover", dragoverHandler);
-  dropItem.addEventListener("dragleave", (e) => {
-    e.target.classList.remove("active");
-  });
-  dropItem.addEventListener("drop", dropHandler);
-  dropItem.addEventListener("dragstart", (ev) => {
-    console.log();
-    const dragItem = document.querySelector(".dragItem");
-    dropItem.prepend(dragItem);
-    ev.dataTransfer.setData("image", ev.target.id);
-    ev.target.classList.remove("active");
-    console.log(ev.target);
-  });
+  dropItem.addEventListener("dragover", dragOver);
+  dropItem.addEventListener("dragenter", dragEnter);
+  dropItem.addEventListener("dragleave", dragLeave);
+  dropItem.addEventListener("drop", dragDrop);
 });
+
+let isDragged = false;
+
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+}
+
+function dragEnd() {
+  this.className = "image dragItem ";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " active";
+}
+
+function dragLeave() {
+  this.className = "dropItem";
+}
+
+function dragDrop() {
+  this.className = "dropItem";
+  this.append(dragItem);
+}
